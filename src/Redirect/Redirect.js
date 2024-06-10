3ca3c618828acafe7098582720a4b0694400b305
bazhenak <bazhena.kutergina@gmail.com>
2024-06-10 16:22:49 +0300

    redirect added

--- src/Redirect/Redirect.js
+++ src/Redirect/Redirect.js
@@ -1,28 +1,44 @@
+// import React from "react";
+// import { useNavigate } from "react-router-dom";
+// import './Redirect.css'
+// import {Link} from "react-router-dom";
+//
+//
+//
+// function Redirect() {
+//
+//     let navigate = useNavigate();
+//     const routeChange = () => {
+//         let path = `newPath`;
+//         navigate(path)
+//
+//         return (
+//             <div className='contacts-page-container'>
+//                 <p>просто параграф</p>
+//                 <button className='button' onClick={routeChange}>
+//                     На главную
+//                 </button>
+//             </div>
+//         );
+//     }
+// }
+//
+// export default Redirect;
+
+
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import './Redirect.css'
-import {Link} from "react-router-dom";
+
+function GetRedirected() {
 
 
+    return (
+        <div>
+            <button className='button' onClick={event =>  window.location.href='../Home/Home.js'}>На главную</button>
+        </div>
+    );
+};
 
-function Redirect() {
 
-    let navigate = useNavigate();
-    const routeChange = () => {
-        let path = `newPath`;
-        navigate(path)
-
-        return (
-            <div className='contacts-page-container'>
-                <p>fjhgjhg</p>
-                <button className='button' onClick={routeChange}>
-                    На главную
-                </button>
-            </div>
-        );
-    }
-}
-
-export default Redirect;
+export default GetRedirected;
 
 
